7d98508a1a80aa507f93ad990cc18b23fa1a53f2
John Li <glorious.rain@gmail.com>
2026-03-15 09:55:59 +0800

    chore: redeploy to cloudflare pages

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,24 +1,19 @@
-import { createRequestHandler } from "@react-router/cloudflare";
+import { createRequestHandler } from "react-router";
 
 // @ts-ignore - server build is generated at build time
 import * as build from "../build/server/index.js";
 
-const handler = createRequestHandler(build);
+const handler = createRequestHandler(build, "production");
 
 export const onRequest: PagesFunction = async (context) => {
-  const { request, next, env } = context;
-  const url = new URL(request.url);
+  const url = new URL(context.request.url);
 
-  // If the request is for a static asset, pass it through to the static assets server
-  if (
-    url.pathname.startsWith("/assets/") ||
-    url.pathname === "/favicon.ico" ||
-    url.pathname === "/robots.txt"
-  ) {
-    return next();
+  // Serve static assets from the build/client directory
+  if (url.pathname.startsWith("/assets/") || url.pathname === "/favicon.ico") {
+    return context.next();
   }
 
-  return handler(request, {
-    cloudflare: { env, ctx: context },
+  return handler(context.request, {
+    cloudflare: { env: context.env, ctx: context },
   });
 };
