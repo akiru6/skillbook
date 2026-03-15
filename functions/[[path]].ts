6f3d5594ac1fc8b86cc6297226906a90dc052d77
John Li <glorious.rain@gmail.com>
2026-03-15 09:45:53 +0800

    fix: skip assets in pages function to allow static file serving

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -6,6 +6,13 @@ import * as build from "../build/server/index.js";
 const handler = createRequestHandler(build, "production");
 
 export const onRequest: PagesFunction = async (context) => {
+  const url = new URL(context.request.url);
+
+  // Serve static assets from the build/client directory
+  if (url.pathname.startsWith("/assets/") || url.pathname === "/favicon.ico") {
+    return context.next();
+  }
+
   return handler(context.request, {
     cloudflare: { env: context.env, ctx: context },
   });
