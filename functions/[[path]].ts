fc368021688b27f263bfd0043cde09b20bd69570
John Li <glorious.rain@gmail.com>
2026-03-15 09:24:09 +0800

    fix: use pages_build_output_dir for Cloudflare Pages compatibility

--- functions/[[path]].ts
+++
@@ -1,9 +0,0 @@
-import { createRequestHandler } from "@react-router/cloudflare";
-// @ts-ignore - build 文件夹在构建时才会生成
-import * as build from "../build/server/index.js";
-
-const handleRequest = createRequestHandler(build);
-
-export const onRequest: PagesFunction = (context) => {
-  return handleRequest(context.request, context.env, context);
-};
